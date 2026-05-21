97ea1e1464bec5fc2e40aeaf8b7533b7cf0b8667
rui0428 <kurorui0428@icloud.com>
2026-05-21 17:47:59 +0900

    [Add]Pusherに物理挙動を追加
    Pusherオブジェクトに物理挙動を追加した

--- Assets/Scripts/Pusher.cs
+++ Assets/Scripts/Pusher.cs
@@ -6,11 +6,14 @@ public class Pusher : MonoBehaviour
     public float speed = 1f;
     public float movePower = 5f;
     private Vector3 startPosition;
+    private Rigidbody rb;
+    
     // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    　void Start()
     {
-        startPosition = this.transform.position;
         Debug.Log("ゲームが開始した");
+        // 自身に付いているgidbodyコンポーネントを取得して、変数rbに入れる
+        rb = this.GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
@@ -18,6 +21,7 @@ public class Pusher : MonoBehaviour
     {
         float z = Mathf.Sin(Time.time * speed) * movePower;
         // 自身のローカル座標の位置を　最初の位置情報に　ｚ（Sin波の変動値）を加算して返す
-        this.transform.localPosition = startPosition + new Vector3(0, 0, z);
+        //this.transform.localPosition = startPosition + new Vector3(0, 0, z);
+        rb.linearVelocity =new Vector3(0, 0, z);
     }
 }
