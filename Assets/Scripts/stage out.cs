c39e31a1a9d2bf9fe2fc70592abc59858f66b608
Fujii0603 <ren-san.f-03@au.com>
2026-06-18 18:39:14 +0900

    Textmeshproを追加

--- Assets/Scripts/stage out.cs
+++ Assets/Scripts/stage out.cs
@@ -1,24 +1,25 @@
-using TMPro;
 using UnityEngine;
+using TMPro;
 
 public class stageout : MonoBehaviour
 {
-    //  変数Scoreの値をUIとして表示する
+    private int Score=0;
     public TextMeshProUGUI scoreText;
 
-    //　アクセス修飾子 型　変数名
-    private int Score;
-
     // このコードをアタッチしたオブジェクトに他のオブジェクトがすり抜けた時に呼ばれる
+    // かつ他のオブジェクトがすり抜けた時に中の処理を行うイベント関数
     void OnTriggerEnter(Collider other)
     {
+       Score += 1;
 
 
-        Score = Score + 1;
-        Debug.Log($"スコア：{Score}");
 
-        //Debug.Log($"{ other.name}がすり抜けた");
-        // すり抜けたオブジェクトを破棄
-        Destroy(other.gameObject);
+       scoreText.text = $"スコア:{Score}";
+
+
+       //変数名　otherってなに？
+       Debug.Log($"{ other.name}がすり抜けた");
+       // すり抜けたオブジェクトを破棄
+       Destroy(other.gameObject);
     }
 }
