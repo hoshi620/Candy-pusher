a4d79e4c478ead07b7c1766a8b96cd2ee978c766
zckukkii <kukkii4262@gmail.com>
2026-07-02 18:40:44 +0900

    candyのランダム化

--- Assets/Scripts/CreateCandy.cs
+++ Assets/Scripts/CreateCandy.cs
@@ -3,18 +3,35 @@ using UnityEngine.InputSystem;
 
 public class CreateCandy : MonoBehaviour
 {
-    private int CandyCount = 0;
 
+    private int CandyCount = 0;
     public GameObject CandyPrefab;
+    public GameObject[] CandyPrefabs;
+    float time = 0f;
     void AddCandy()
     {
         CandyCount = CandyCount + 1;
         Debug.Log(CandyCount);
 
-        //オブジェクトの生成方法
-        //型　変数　＝　初期値(Instantiateで作られたオブジェクト)
-        GameObject creatPrefab = Instantiate(CandyPrefab);
-        // position
+        int rand = Random.Range(0, 100);
+        int candyType = 0;
+        
+        if(rand < 5)
+        {
+            candyType = 0;
+        }
+        else if(rand < 80)
+        {
+            candyType = 1;
+        }
+        else if(rand < 99)
+        {
+            candyType = 2;
+        }
+            //オブジェクトの生成方法
+            //型　変数　＝　初期値(Instantiateで作られたオブジェクト)
+            GameObject creatPrefab = Instantiate(CandyPrefabs[candyType]);
+        
         creatPrefab.transform.position = this.transform.position;
     }
     void Start()
@@ -27,7 +44,7 @@ public class CreateCandy : MonoBehaviour
 
     void Update()
     {
-        if (Keyboard.current.spaceKey.isPressed)
+        if (Keyboard.current.spaceKey.wasPressedThisFrame)
         {
             AddCandy();
         }
--- Assets/Scripts/pusher2.cs
+++ Assets/Scripts/pusher2.cs
@@ -1,6 +1,24 @@
 using UnityEngine;
 
-public class pusher2
+public class pusher2: MonoBehaviour
 {
+
+    private Vector3 startPosition;
     
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        startPosition = this.transform.position;
+        Debug.Log("ÉQÅ[ÉÄÇ™äJénÇµÇΩ");
+        
+        startPosition = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float x = Mathf.Sin(Time.time * 1f)*3f;
+        this.transform.position = startPosition + new Vector3(x, 0, 0);
+    }
 }
